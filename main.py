cf4c52c68ecfa17dc998b1c51b8a8186404320cd
binthon <jakubmedyn@wp.pl>
2024-06-29 21:33:36 +0200

    pwm

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-import pandas
-import numpy
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+pin = 17
+
+GPIO.setup(pin,GPIO.OUT)
+led_pwm = GPIO.PWM(pin, 100)
+
+led_pwm.start(0)
+
+try:
+    while True:
+        for dc in range(0, 101, 5):
+            led_pwm.ChangeDutyCycle(dc)
+            time.sleep(1)
+        for dc in range(100, -1, -5):
+            led_pwm.ChangeDutyCycle(dc)
+            time.sleep(1)
+except KeyboardInterrupt:
+    print("\n End")
+finally:
+    led_pwm.stop()
+    GPIO.cleanup()
+
