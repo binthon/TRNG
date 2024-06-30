18d9ee8f8544dd99c0a931a83d7bccbe4ac29324
binthon <jakubmedyn@wp.pl>
2024-06-30 14:45:40 +0200

    voltage reader

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import RPi.GPIO as GPIO
 import time
+import spidev
+
+# Ustawienie trybu numeracji pinów na BCM (numeracja GPIO)
 GPIO.setmode(GPIO.BCM)
 
-pin = 17
+# Ustawienie GPIO23 jako wyjście
+GPIO.setup(23, GPIO.OUT)
 
-GPIO.setup(pin,GPIO.OUT)
-led_pwm = GPIO.PWM(pin, 100)
+# Ustawienie stanu wysokiego na GPIO23
+GPIO.output(23, GPIO.HIGH)
 
-led_pwm.start(0)
+# Inicjalizacja SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1350000
 
-try:
-    while True:
-        for dc in range(0, 101, 5):
-            led_pwm.ChangeDutyCycle(dc)
-            time.sleep(1)
-        for dc in range(100, -1, -5):
-            led_pwm.ChangeDutyCycle(dc)
-            time.sleep(1)
-except KeyboardInterrupt:
-    print("\n End")
-finally:
-    led_pwm.stop()
-    GPIO.cleanup()
+# Funkcja do odczytu kanału z MCP3008
+def read_channel(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
 
+# Odczyt napięcia z kanału 0 (R1 podłączony do CH0 MCP3008) przez 10 sekund
+channel = 0
+for i in range(10):
+    adc_value = read_channel(channel)
+    voltage = (adc_value * 3.3) / 1023  # Przeskalowanie wartości ADC do napięcia
+    print(f"Czas: {i + 1}s, Odczytana wartość ADC: {adc_value}, Napięcie na R1: {voltage:.2f} V")
+    time.sleep(1)
+
+# Wyłączenie diody (ustawienie stanu niskiego na GPIO23)
+GPIO.output(23, GPIO.LOW)
+
+# Wyłączenie trybu pull-up na GPIO23 (jeśli jest ustawiony)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
+
+# Czyszczenie ustawień GPIO
+GPIO.cleanup()
+
+# Zamknięcie SPI
+spi.close()
